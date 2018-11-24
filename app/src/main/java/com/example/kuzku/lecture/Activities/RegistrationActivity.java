9ea463f7040f9ddc579704e44f76e139f00b849e
Nikolay9919 <kolya2pora>
2018-11-24 19:18:37 +0200

    a

---
+++ app/src/main/java/com/example/kuzku/lecture/Activities/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.example.kuzku.lecture.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.kuzku.lecture.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lectures);
+    }
+}
